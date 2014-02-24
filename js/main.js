3bf8d98ef0dc7317be20aa3322a7a115a946a7ab
MaxWofford <max.speed.wofford@gmail.com>
2014-02-23 20:16:28 -0800

    Added form filler and footer copyright

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 
+
+POST https://www.googleapis.com/fusiontables/v1/tables/1e7y6mtqv892233322222_bbbbbbbbb_CvWhg9gc/import
+Authorization: /* auth token here */
+Content-Type: application/octet-stream
+
+Moth, 218 W. Oak Street CA 90245
